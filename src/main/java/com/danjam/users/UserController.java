cda34c9e8c70189cdc5da1f0c776643a5dcd0340
kimdohee58 <kimdohee58@gmail.com>
2024-08-07 11:40:28 +0900

    completed-login, register

--- src/main/java/com/danjam/users/UserController.java
+++ src/main/java/com/danjam/users/UserController.java
@@ -1,10 +1,16 @@
 package com.danjam.users;
 
-import com.danjam.logInSecurity.UserDetail;
+
+import com.danjam.users.Users;
+import com.danjam.users.UsersDto;
+import com.danjam.users.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,42 +31,45 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping("validate")
-    public Map<String, Object> validate(@RequestBody String email) {
-        Map<String, Object> resultMap = new HashMap<>();
-        // 값 이상하게 넘어와서 replace 해줌
-        email = email.replace("%40", "@");
-        email = email.replace("=", "");
-
-        if (usersService.findByEmail(email) == null) {
-            resultMap.put("result", "success");
-        } else {
-            resultMap.put("result", "fail");
-        }
-        return resultMap;
-    }
-
     @PostMapping("signUp")
     public HashMap<String, Object> signUp(@RequestBody UsersDto usersDto) {
         HashMap<String, Object> resultMap = new HashMap<>();
         System.out.println("회원가입 접속");
+        System.out.println(usersDto.getName());
+        System.out.println(usersDto.getPassword());
+        System.out.println(usersDto.getEmail());
+        System.out.println(usersDto.getPhoneNum());
 
-        try {
+        // 중복 검사
+        List<Users> list = usersService.findAll();
+        for (Users user : list) {
+            if(user.getEmail().equals(usersDto.getEmail())) {
+                resultMap.put("result", "fail");
+                return resultMap;
+            }
+        }
+
+        try{
             usersDto.setPassword(passwordEncoder.encode(usersDto.getPassword()));
+            Users users = usersDto.toEntity();
             System.out.println("회원가입 성공");
-            usersService.save(usersDto.toEntity());
+            System.out.println(users.getName());
+            System.out.println(users.getPassword());
+            System.out.println(users.getEmail());
+            System.out.println(users.getPhoneNum());
+            usersService.save(users);
             resultMap.put("result", "success");
-        } catch (Exception e) {
+        }catch (Exception e) {
             e.printStackTrace();
             resultMap.put("result", "fail");
         }
         return resultMap;
     }
 
-    @RequestMapping("authSuccess")
-    public ResponseEntity<Map<String, Object>> authSuccess(@AuthenticationPrincipal UserDetail userDetail) {
+    @RequestMapping("auth/authSuccess")
+    public ResponseEntity<Map<String, Object>> authSuccess(Authentication authentication) {
         HashMap<String, Object> response = new HashMap<>();
-        Users user = userDetail.getUser();
+        Users user = (Users) authentication.getPrincipal();
 
         response.put("result", "success");
         response.put("id", user.getId());
@@ -70,7 +79,7 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
-    @RequestMapping("authFailure")
+    @RequestMapping("auth/authFailure")
     public ResponseEntity<Map<String, Object>> authFailure() {
         HashMap<String, Object> response = new HashMap<>();
 
