813fa40b618a8fa3ddf389a46d653acab13c100f
kimdohee58 <kimdohee58@gmail.com>
2024-08-07 12:16:12 +0900

    valid

--- src/main/java/com/danjam/users/UserController.java
+++ src/main/java/com/danjam/users/UserController.java
@@ -31,6 +31,21 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @PostMapping("validate")
+    public Map<String, Object> validate(@RequestBody String email) {
+        Map<String, Object> resultMap = new HashMap<>();
+        // 값 이상하게 넘어와서 replace 해줌
+        email = email.replace("%40", "@");
+        email = email.replace("=", "");
+
+        if (usersService.findByEmail(email) == null) {
+            resultMap.put("result", "success");
+        } else {
+            resultMap.put("result", "fail");
+        }
+        return resultMap;
+    }
+
     @PostMapping("signUp")
     public HashMap<String, Object> signUp(@RequestBody UsersDto usersDto) {
         HashMap<String, Object> resultMap = new HashMap<>();
