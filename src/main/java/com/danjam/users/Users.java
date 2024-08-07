8162411e9be1cc019e71112be395fbac543b9f14
kimdohee58 <kimdohee58@gmail.com>
2024-08-07 10:55:39 +0900

    login completed-valid, register, login

---
+++ src/main/java/com/danjam/users/Users.java
@@ -0,0 +1,69 @@
+package com.danjam.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Table(name = "users")
+@DynamicInsert // default
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone_number", nullable = false)
+    private int phoneNum;
+
+    @Enumerated(EnumType.STRING) // enumtype.string 옵션 사용하면 enum 이름 그대로 db에 저장
+    @Column(name = "role", nullable = true)
+    private Role role;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Date createDate;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Date updateDate;
+
+    @Column(name = "status")
+    @ColumnDefault("Y")
+    private String status;
+
+    @Builder
+    public Users(int id, String email, String password, String name, int phoneNum, Role role, Date createDate, Date updateDate, String status) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.role = role;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+        this.status = status;
+    }
+
+    public Users(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.role = getRole();
+    }
+}
