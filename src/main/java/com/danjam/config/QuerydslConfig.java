360b7e362673bff4c247144b77e32c1e80384d47
kimdohee58 <kimdohee58@gmail.com>
2024-08-16 16:06:25 +0900

    test final

--- src/main/java/com/danjam/config/QuerydslConfig.java
+++ src/main/java/com/danjam/config/QuerydslConfig.java
@@ -2,11 +2,14 @@ package com.danjam.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QueryDslConfig {
+    @PersistenceContext // EntityManager를 빈으로 주입할 때 사용하는 어노테이션
+    private EntityManager em;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
